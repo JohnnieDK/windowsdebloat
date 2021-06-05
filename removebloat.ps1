c3afdd6eeb285b51b3ccf2565f84a16d1f51835a
Johnnie Gjedved <46320367+JohnnieDK@users.noreply.github.com>
2021-06-05 08:50:47 +0200

    alarm, 3dbuilder, 3dprint and camera will no longer be removed

--- removebloat.ps1
+++ removebloat.ps1
@@ -1,8 +1,9 @@
 # This will remove alot of windows default installed apps 
 # Run as administrator
 # TODO: some of these have changed names or is no longer being installed
+# NOTE: These can be reinstalled from the MS store.
 
-$Apps = @("Cortana", "OfficeHub", "Messaging", "Solitaire", "3DBuilder", "Bing", "ConnectivityStore", "Getstarted", "Messaging",  "Skype", "Xbox", "Zune", "BioEnrollment", "ContactSupport", "windowsmaps", "Twitter", "NetworkSpeedTest", "Duolingo", "Pandora", "Print3D", "AdobePhotoshopExpress", "WindowsCamera", "Microsoft3DViewer", "windowsphone", "Microsoft.YourPhone", "windowsalarms", "windowscommunicationsapps", "windowscamera", "onenote", "people")
+$Apps = @("Cortana", "OfficeHub", "Messaging", "Solitaire", "3DBuilder", "Bing", "ConnectivityStore", "Getstarted", "Messaging",  "Skype", "Xbox", "Zune", "BioEnrollment", "ContactSupport", "windowsmaps", "Twitter", "NetworkSpeedTest", "Duolingo", "Pandora", "AdobePhotoshopExpress", "windowsphone", "Microsoft.YourPhone", "windowscommunicationsapps", "onenote", "people")
 
 ForEach($App in $Apps) {
     $AppPattern = "*$App*"
